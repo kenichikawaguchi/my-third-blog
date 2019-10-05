c7c00b56eb9905c822c5f90eb55ae2ca994eb95d
kenichikawaguchi <xianzaikaishiba@gmail.com>
2019-10-05 21:11:20 +0900

    ALLOWED_HOSTS modified

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'pv4fvnmb83dg$4qfnu*^eqzif#$_!m@6-nsk4v$1)evwf^+u*1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', '.herokuapp.com']
 
 
 # Application definition
